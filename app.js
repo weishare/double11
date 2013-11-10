dbffc2cfdf8fd87efed688427e498f875ea00e74
U-hh-PC\hh <hh@hh-PC.(none)>
2013-11-10 14:46:17 +0800

    commit all

---
+++ app.js
@@ -0,0 +1,58 @@
+
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+
+//年双十一活动商品清单-价格提前曝光-已解密
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon(path.join(__dirname, 'public/favicon.ico'))); 
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+app.get('/tag/:tag', routes.index);
+app.get('/brands/:brands', routes.brands);
+app.get('/search', routes.search);
+app.get('/', routes.index);
+
+
+
+
+//var obj = xlsx.parse(fs.readFileSync(__dirname + '/20131111.xlsx')); // parses a buffer
+//var obj = xlsx.parse(__dirname + '/20131111.xlsx'); // parses a file
+
+
+
+
+
+
+
+
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
---
+++ routes/index.js
@@ -0,0 +1,89 @@
+
+/*
+ * GET home page.
+ */
+
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://zhfsxtx:zhf19870917@localhost/double11');
+
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'连接错误:'));
+db.once('open',function(){
+    console.log('open double11');
+});
+
+var Schema = mongoose.Schema;
+
+var Double11Schema = Schema({
+        pinpai: String,
+        name: String,
+        price1: Number,//现售价
+        price2: Number,//专柜价
+        price3: Number,//历史最低售价
+        price4: Number,//11.11价格
+        url: String,//连接
+        tag: String//分类
+    });
+
+
+var TagSchema = Schema({
+        tag: String,
+        brands: [String]
+    });
+
+
+
+var Model = mongoose.model("product", Double11Schema);
+var BrandsModel = mongoose.model("tagBrands", TagSchema);
+
+
+exports.index = function(req, res){
+
+	console.log(req.cookies)
+	var tag = req.params.tag || "女装";
+
+	BrandsModel.findOne({tag : tag}).exec(function(err, Onebrands){
+		 Model.find({pinpai: Onebrands.brands[0]}).exec(function(err, list){
+		 	if(err) console.log(err)
+		 		req.session.tag = tag;
+		 		res.render('index', { title: '陪你买——' + tag, list: list, brands: Onebrands.brands});
+		 });
+	})
+};
+
+
+exports.brands = function(req, res){
+	if (req.params.brands ) {
+		Model.find({pinpai: req.params.brands}).exec(function(err, list){
+		 	if(err) console.log(err)
+		 		res.render('products', { list: list});
+		 });
+	};
+};
+
+
+
+exports.search = function(req, res){
+	
+	if (req.query.key ) {
+		var where = 'this.name.indexOf("'+ req.query.key + '") >= 0';
+
+		if (req.query._id) {
+
+			Model.find({$where:where}).where('_id').lt(req.query._id).limit(100).exec(function(err, list){
+			 	if(err) console.log(err)
+			 		res.render('moreproducts', { list: list});
+			 });
+
+		} else {
+			Model.find({$where:where}).limit(100).exec(function(err, list){
+			 	if(err) console.log(err)
+			 		res.render('search', { list: list, more :( list.length === 100)});
+			 });
+
+		}
+
+	};
+};
